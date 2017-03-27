ccd09bb7f912a7be5aa0cd72f33d0a6924c01270
Tyler Anderson <tba109@psu.edu>
2017-03-27 18:19:04 -0400

    Added VREF1 tempco info

---
+++ vref1_tempco_Mar27_2017/plot_keysight_34470a.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+import datetime
+import sys
+
+f = open('../../tmc_cal_data/keysight_34470a_Mar27_2017_tempco_vref1.csv','r')
+time_meas = []
+v_meas = []
+for line in f:
+    strtime = line.split(',')[0]
+    tstamp = time.mktime(datetime.datetime.strptime(strtime,'%Y-%m-%d %H:%M:%S.%f').timetuple())
+    time_meas.append(tstamp)
+    v_meas.append(float(line.split(',')[1]))
+
+for x,y in zip(time_meas,v_meas):
+    print x,y
+
+plt.plot(time_meas,v_meas)
+plt.show()
+
+f.close()
+
