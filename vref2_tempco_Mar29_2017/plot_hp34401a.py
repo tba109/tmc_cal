818a876d59c9e836c99539c8e2a42abc81819f37
Tyler Anderson <tba109@psu.edu>
2017-03-29 14:16:36 -0400

    Added vref2 tempco data

---
+++ vref2_tempco_Mar29_2017/plot_hp34401a.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+import datetime
+
+f = open('../../tmc_cal_data/hp34401a_2017-03-29_09_24_47_084799.txt','r')
+time_meas = []
+v_meas = []
+for line in f:
+    strtime = line.split()[0]
+    tstamp = time.mktime(datetime.datetime.strptime(strtime,'%Y-%m-%d_%H:%M:%S:%f').timetuple())
+    time_meas.append(tstamp)
+    v_meas.append(float(line.split()[1]))
+
+for x,y in zip(time_meas,v_meas):
+    print x,y
+
+plt.plot(time_meas,v_meas)
+plt.show()
+
+f.close()
+
